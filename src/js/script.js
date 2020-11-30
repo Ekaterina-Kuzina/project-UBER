93fae8e233961805451623f6da7556a98b94e601
Ekaterina <katya.cuzina2011@yandex.ru>
2020-11-30 10:59:17 +0300

    finish

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,27 @@
 'use strict'
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu');
+const menuItem = document.querySelectorAll('.menu_item');
 
 
 hamburger.addEventListener('click', () => {
     if (hamburger.classList.contains('hamburger_active')) {
-        hamburger.classList.remove('hamburger_active');
-        menu.classList.remove('menu_active');
+        remove(hamburger, menu);
     } else {
-        hamburger.classList.add('hamburger_active');
-        menu.classList.add('menu_active');
+        add(hamburger, menu);
     }
 });
+menuItem.forEach((item) => {
+    item.addEventListener('click', () => {
+        remove(hamburger, menu);
+    });
+});
+
+function remove(hamburger,menu){
+    hamburger.classList.remove('hamburger_active');
+    menu.classList.remove('menu_active');
+}
+function add(hamburger,menu){
+    hamburger.classList.add('hamburger_active');
+    menu.classList.add('menu_active');
+}
